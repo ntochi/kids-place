0d180d4b1a513ecf90b82a4cea8c0578fea25c8d
Nancy Tochi O <nancytochi24@gmail.com>
2020-08-05 10:18:14 -0400

    Show more info about a product

--- routes/products.js
+++ routes/products.js
@@ -2,6 +2,7 @@ const express = require("express");
 const router = express.Router();
 const Product = require('../models/product');
 
+
 // INDEX: display a list of all products
 router.get("/shop", function (req, res){
     // Get all products from database
@@ -12,6 +13,19 @@ router.get("/shop", function (req, res){
 			res.render("products/index", {product: allProducts});
         }
     })
+});
+
+// SHOW: show more info about a product
+router.get("/shop/:id", function (req, res){
+    // Find product by ID
+    Product.findById(req.params.id, function(err, foundProduct){
+        if (err) {
+            console.log(err);
+        } else {
+            res.render("products/show", {product: foundProduct});
+        }
+    });
+
 });
 
 
