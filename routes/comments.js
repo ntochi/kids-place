544286c42e14ce65b123e421c113bf74fd3397eb
Nancy Tochi O <nancytochi24@gmail.com>
2020-08-18 09:49:42 -0400

    Feature: create & retrieve comments on product

---
+++ routes/comments.js
@@ -0,0 +1,42 @@
+const express = require("express");
+const router = express.Router();
+const Product = require('../models/product');
+const Comment = require('../models/comment');
+
+// NEW: display form to make a new comment
+router.get("/shop/:id/comments/new", function (req, res){
+    // Find product by ID
+    Product.findById(req.params.id, function(err, foundProduct){
+        if (err) {
+            console.log(err);
+        } else {
+			res.render("comments/new", {product: foundProduct});
+        }
+    })
+});
+
+// CREATE: add new comment to database
+router.post("/shop/:id/comments", function(req, res){
+    // Find product by ID
+    Product.findById(req.params.id, function(err, foundProduct){
+        if (err) {
+            console.log(err);
+        } else {
+            // Create new comment
+            Comment.create(req.body.comment, function(err, comment){
+				if (err) {
+					console.log(err)
+				} else {
+					//Associate comment with product, then redirect
+					foundProduct.comments.push(comment);
+					foundProduct.save();
+					res.redirect("/shop/" + foundProduct._id);
+				}
+			});
+        }
+    })
+
+});
+
+
+module.exports = router;
