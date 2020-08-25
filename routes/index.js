090184e8e23722b83c3d7fae94867b08195df93e
Nancy Tochi O <nancytochi24@gmail.com>
2020-08-25 09:47:32 -0400

    Add authentication routes

--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,52 @@
 const express = require("express");
 const router = express.Router();
+const passport = require("passport");
+const User = require('../models/user');
 
-
+// Landing route
 router.get("/", function (req, res){
     res.render("landing");
 });
 
+// User profile route
+router.get("/user", function (req, res){
+    res.render("user/user");
+});
 
+// Register route
+router.get("/register", function (req, res){
+    res.render("user/register");
+});
 
+// Handle register logic
+router.post("/register", function(req, res){
+    var newUser = new User({username: req.body.username});
+    User.register(newUser, req.body.password, function(err, newUser){
+        if(err){
+            console.log(err);
+            res.redirect("/register");
+         }
+        passport.authenticate("local")(req, res, function(){
+            res.redirect("/shop");
+        });
+    });});
+
+// Login route
+router.get("/login", function (req, res){
+    res.render("user/login");
+});
+
+// Handle login logic
+router.post("/login", passport.authenticate("local", {successRedirect: "/shop", failureRedirect: "/login"}), function(req, res){
+
+});
+
+//Logout route
+router.get("/logout", function(req, res){
+	//destroy all the user data in the session
+	req.logout();
+	res.redirect("/login");
+});
 
 
 
